c121f7a8b553ae9002cab75030a7606316acf9c9
gangwolfmail <129858122+gangwolfmail@users.noreply.github.com>
2024-02-05 20:59:41 +0200

    init hw #8

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-const checkInput = () => {
-  output.textContent =
-    input.value.trim() !== "" ? input.value.trim() : "Anonymous";
-};
-
-input.addEventListener("input", checkInput);
